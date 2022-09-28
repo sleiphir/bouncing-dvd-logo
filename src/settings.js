5fcc40af385838b3a251144d864c78332cf12866
Simon Lasbrugnas <simon.lasbrugnas@gmail.com>
2022-09-28 17:08:10 +0200

    Remove unnecessary comments

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,3 @@
-// Settings toggle button
 document
   .querySelector(".settings-toggle")
   .addEventListener("click", () => {
@@ -7,28 +6,24 @@ document
     settings.style.display = settings.style.display === "block" ? "none" : "block"
   })
 
-// Background color
 document
   .querySelector("#background-color")
   .addEventListener("input", (event) => {
     document.body.style.background = event.target.value
   })
 
-// Logo color
 document
   .querySelector("#logo-color")
   .addEventListener("input", (event) => {
     document.querySelector(".logo > svg").style.fill = event.target.value
   })
 
-// Speed X
 document
   .querySelector("#speed-x")
   .addEventListener("input", (event) => {
     settings.speed.x = settings.speed.x >= 0 ? parseInt(event.target.value) : parseInt(-1 * event.target.value)
   })
 
-// Speed Y
 document
   .querySelector("#speed-y")
   .addEventListener("input", (event) => {
